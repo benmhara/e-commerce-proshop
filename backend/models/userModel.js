2c2c854c1bf7a44fba51a00b8e28d5814c86d45b
benmhara <benmharamalek@gmail.com>
2023-07-15 22:48:39 +0100

    Models

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+const { Schema, model } = mongoose
+
+const reviewSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const userModel = model('User', userSchema)
+
+export default userModel
