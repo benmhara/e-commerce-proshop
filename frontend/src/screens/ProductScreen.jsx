6b313b1362828d5a2e2334fa7f8bea62b078a6cf
benmhara <benmharamalek@gmail.com>
2023-07-17 02:47:02 +0100

    Loader & Message

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -10,6 +10,8 @@ import {
   ListGroupItem,
 } from 'react-bootstrap'
 import Rating from '../components/Rating'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const ProductScreen = () => {
   const { id } = useParams()
@@ -21,9 +23,9 @@ const ProductScreen = () => {
         Go Back
       </Link>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>{error.data?.message || error.error}</div>
+        <Message variant="danger">{error.data?.message || error.error}</Message>
       ) : (
         <>
           <h1>Latest products</h1>
