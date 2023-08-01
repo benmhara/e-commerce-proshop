30c2e443dbc3da0a3a7d14b6a0f90cb736cb1980
benmhara <benmharamalek@gmail.com>
2023-08-01 08:43:25 +0100

    Frontend Authentication

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -13,10 +13,19 @@ const authSlice = createSlice({
     setCredentials: (state, action) => {
       state.userInfo = action.payload
       localStorage.setItem('userInfo', JSON.stringify(action.payload))
+
+      const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000 // 30 days
+      localStorage.setItem('expirationTime', expirationTime)
+    },
+    logout: (state, action) => {
+      state.userInfo = null
+      // NOTE: here we need to also remove the cart from storage so the next
+      // logged in user doesn't inherit the previous users cart and shipping
+      localStorage.clear()
     },
   },
 })
 
-export const { setCredentials } = authSlice.actions
+export const { setCredentials, logout } = authSlice.actions
 
 export default authSlice.reducer
