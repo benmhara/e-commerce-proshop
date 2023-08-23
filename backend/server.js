e0b3ee7eb348349447ab2b9fba0121e7dc069a85
benmhara <benmharamalek@gmail.com>
2023-08-24 00:17:16 +0100

    initial deployment

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 dotenv.config()
 import connectDB from './config/db.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const port = process.env.PORT || 5000
 
@@ -16,35 +16,38 @@ connectDB()
 
 const app = express()
 
-// Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-// Cookie parser middleware
 app.use(cookieParser())
-
-app.get('/', (req, res) => {
-  res.send('Server is ready')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-// PayPal client id
 app.get('/api/config/paypal', (req, res) =>
-  res.send({
-    clientId: process.env.PAYPAL_CLIENT_ID,
-  })
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 )
 
-// Make uploads folder static
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve()
+  app.use('/uploads', express.static('/var/data/uploads'))
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
-// Error handling middleware
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  )
+} else {
+  const __dirname = path.resolve()
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server runnig on port ${port}`))
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+)
