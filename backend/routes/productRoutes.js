e85485d204b9fb2fbdd46f28cb7d427db253c522
benmhara <benmharamalek@gmail.com>
2023-08-23 23:44:12 +0100

    Admin Functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,17 @@ const router = express.Router()
 import {
   getProducts,
   getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 } from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.get('/', getProducts)
-
-router.get('/:id', getProductById)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct)
 
 export default router
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -1,19 +1,60 @@
-import { PRODUCTS_URL } from '../constants'
+import { PRODUCTS_URL, UPLOAD_URL } from '../constants'
+
 import { apiSlice } from './apiSlice'
+import { useDeliverOrderMutation } from './ordersApiSlice'
 
 export const productsApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
     getProducts: builder.query({
-      query: () => PRODUCTS_URL,
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
       providesTags: ['Product'],
       keepUnusedDataFor: 5,
     }),
     getProduct: builder.query({
-      query: id => `${PRODUCTS_URL}/${id}`,
-      providesTags: ['Product'],
+      query: id => ({
+        url: `${PRODUCTS_URL}/${id}`,
+      }),
       keepUnusedDataFor: 5,
+    }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    updateProduct: builder.mutation({
+      query: data => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Products'],
+    }),
+    uploadProductImage: builder.mutation({
+      query: data => ({
+        url: `${UPLOAD_URL}`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    deleteProduct: builder.mutation({
+      query: productId => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: 'DELETE',
+      }),
+      providesTags: ['Product'],
     }),
   }),
 })
 
-export const { useGetProductsQuery, useGetProductQuery } = productsApiSlice
+export const {
+  useGetProductsQuery,
+  useGetProductQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+  useDeleteProductMutation,
+} = productsApiSlice
